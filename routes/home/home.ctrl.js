40bd8586e8bdad7e9548e043187aaff9ce09c51b
jungukchan <wkchan2254@gmail.com>
2025-01-13 11:13:43 +0900

    first commit

---
+++ routes/home/home.ctrl.js
@@ -0,0 +1,17 @@
+"use strict";
+
+/// ---- controller ----
+
+const home =  (req, res) => {
+    res.render("home/index");
+};
+
+
+const login = (req,res)=>{
+    res.render("home/login");
+};
+
+module.exports = {
+    home,
+    login,
+};
